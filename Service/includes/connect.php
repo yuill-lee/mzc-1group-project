e4a62a680a47a73ed85e3930ba76a306ca29edf0
now <fluw6@naver.com>
2025-12-24 11:20:11 +0900

    pull&push 테스트 종료 /정상복원입니다

--- Service/includes/connect.php
+++ Service/includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-		$link = mysqli_connect("192.168.65.175", "user02", "user02");
+		$link = mysqli_connect("192.168.65.175", "user01", "user01");
 		mysqli_select_db($link, "test_db");
 		mysqli_query($link, "SET CHARACTER SET utf8");
 		?>
