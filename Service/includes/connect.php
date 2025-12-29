8327b3349b3111bf1d41a9b3d8e49da4040b672d
seowj <tjdnjswhd1@naver.com>
2025-12-29 16:25:11 +0900

    connect.php update.

--- Service/includes/connect.php
+++ Service/includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 // 도커 환경 변수를 읽어오고, 없으면 기본값인 'db'를 사용합니다.
-$host = getenv('DB_HOST') ?: 'db'; 
+$host = getenv('DB_HOST') ?: '<DB-ENDPOINT>'; 
 $user = getenv('DB_USER') ?: 'user01';
-$pass = getenv('DB_PASS') ?: 'user01';
+$pass = getenv('DB_PASS') ?: 'user01password';
 $dbname = getenv('DB_NAME') ?: 'test_db';
 
 $link = mysqli_connect($host, $user, $pass, $dbname);
