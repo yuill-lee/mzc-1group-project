016c4c8c9776313f466b3db040eff5c04db36af2
now <fluw6@naver.com>
2025-12-29 11:30:44 +0900

    save.php 수정( USERS게시판 반영안되는 문제 해결함)

--- Service/save.php
+++ Service/save.php
@@ -1,27 +1,32 @@
 <?php
-		include("includes/connect.php");
+include("includes/connect.php");
 
-		$cat = $_POST['cat'];
-		$cat_get = $_GET['cat'];
-		$act = $_POST['act'];
-		$act_get = $_GET['act'];
-		$id = $_POST['id'];
-		$id_get = $_GET['id'];
+// 1. 모든 Undefined array key 에러 방지
+$cat     = $_POST['cat'] ?? $_GET['cat'] ?? '';
+$cat_get = $_GET['cat'] ?? '';
+$act     = $_POST['act'] ?? $_GET['act'] ?? '';
+$act_get = $_GET['act'] ?? '';
+$id      = $_POST['id'] ?? $_GET['id'] ?? '';
+$id_get  = $_GET['id'] ?? '';
 
-		
-				if($cat == "users" || $cat_get == "users") {
-					$user_name = addslashes(htmlentities($_POST["user_name"], ENT_QUOTES));
-$password = addslashes(htmlentities($_POST["password"], ENT_QUOTES));
-$name = addslashes(htmlentities($_POST["name"], ENT_QUOTES));
+if($cat == "users" || $cat_get == "users") {
+    // 2. 보안 기능 없이 변수만 할당 (Warning 방지)
+    $password  = $_POST["password"] ?? '';
+    $name      = $_POST["name"] ?? '';
+    $email     = $_POST["email"] ?? ''; 
 
+    // 3. 작업 로직
+    if($act == "add") {
+        // 'role' 컬럼이 숫자형이므로 문자 'user' 대신 숫자 1을 삽입
+        mysqli_query($link, "INSERT INTO `users` (`name`, `password`, `email`, `role`) VALUES ('$name', '".md5($password)."', '$email', 1)");
+        
+    } elseif ($act == "edit") {
+        mysqli_query($link, "UPDATE `users` SET `name` = '$name', `email` = '$email' WHERE `id` = '$id'"); 
+        
+    } elseif ($act_get == "delete") {
+        mysqli_query($link, "DELETE FROM `users` WHERE id = '$id_get'");
+    }
 
-				if($act == "add") {
-					mysqli_query($link, "INSERT INTO `users` (  `name` , `password` , `email` ) VALUES ( '".$name."' , '".md5($password)."', '".$email."' ) ");
-				}elseif ($act == "edit") {
-					mysqli_query($link, "UPDATE `users` SET  `name` =  '".$name."' , `email` =  '".$email."'  WHERE `id` = '".$id."' "); 
-					}elseif ($act_get == "delete") {
-						mysqli_query($link, "DELETE FROM `users` WHERE id = '".$id_get."' ");
-					}
-					header("location:"."users.php");
-				}
-				?>
+    header("location: users.php");
+}
+?>
