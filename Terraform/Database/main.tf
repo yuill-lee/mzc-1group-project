7179fb69f3d6c88e3f16f3940d23c6fe31e05f9b
loafer-ahnno <loaferahnno@gmail.com>
2025-12-29 17:46:33 +0900

    add main db backup

--- Terraform/Database/main.tf
+++ Terraform/Database/main.tf
@@ -47,6 +47,7 @@ resource "aws_db_instance" "default" {
 
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
+  backup_retention_period = 7
 
   tags = { Name = "MZC-MySQL-DB" }
 }
